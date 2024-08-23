f11d5c2166abb58a0f3765a69b887b283fee4c04
Abhishek Rathore <29rabhishek@gmail.com>
2024-08-24 00:49:16 +0530

    Update train_vae_multi.py

--- tools/train_vae_multi.py
+++ tools/train_vae_multi.py
@@ -146,9 +146,6 @@ def train(rank, world_size, args):
                 scaler.step(optimizer_g)
                 scaler.update()
                 optimizer_g.zero_grad()
-        print(f"kl loss {kl_losses}")
-        print(f"kl loss {losses}")
-        print(f"kl loss {recon_losses}")
         print(f'GPU[{rank}] epoch: {epoch_idx+1} | Total Loss: {np.mean(losses)} | Recon Loss : {np.mean(recon_loss):.4f} | KL Loss:  {np.mean(kl_losses)}')
         if rank == 0:  # Log only on the main process      
             torch.save(model.module.state_dict(), f"{os.path.join(train_config['task_name'],train_config['vae_autoencoder_ckpt_name'])}_{epoch_idx+1}.pth")
